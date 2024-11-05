4dc14ec262ec958fa2e503ec688b3c6fe8c24370
gestionDeDatos05 <sistemaoficina05@gmail.com>
2024-11-05 14:39:09 -0600

    Config DB

--- Gastos/settings.py
+++ Gastos/settings.py
@@ -78,10 +78,25 @@ WSGI_APPLICATION = 'Gastos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'LZQBrFwMIeXkRuPBLkFcHuXDTMxaTzBs',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },        
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
